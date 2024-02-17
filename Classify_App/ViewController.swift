f14ef227524cdcd907c9d37c5edb554544af29d2
AtsushiHosaka <hosaka2917@gmail.com>
2024-02-17 18:52:04 +0900

    Initial Commit

---
+++ Classify_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Classify_App
+//
+//  Created by 保坂篤志 on 2024/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
